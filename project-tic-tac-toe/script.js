7a2169949b4a1b30c35e15b54336920c0f3e880f
exp1 <gorvkrtianliya@gmail.com>
2023-02-02 14:36:47 +0530

    added tic-tac link to main page

--- project-tic-tac-toe/script.js
+++ project-tic-tac-toe/script.js
@@ -1,14 +1,3 @@
-const winMove = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 4, 5],
-  [6, 7, 8],
-  [2, 4, 6],
-];
-
 const Player = (name, sign) => {
   const playerMoves = [];
 
@@ -31,6 +20,16 @@ const gameBoard = (() => {
   const textDisplay = document.querySelector(".textdisplay");
 
   let gameBoardArr = ["", "", "", "", "", "", "", "", ""];
+  const winMove = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 4, 6],
+  ];
 
   let turn = "p1";
   let player1;
